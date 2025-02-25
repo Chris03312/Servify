368e02dcb5efa566ac1b3eaf4d5ea97fd551db26
Vicmar <vicmarrrrr.2002@gmail.com>
2025-02-25 23:11:40 +0800

    adsadsadsadsada

--- includes/volunteer_sidebar.php
+++ includes/volunteer_sidebar.php
@@ -55,9 +55,9 @@
             <!--HELP DESK-->
             <small class="text-muted">HELP DESK</small>
             <div class="help_desk">
-            <p><i class="bi bi-telephone-fill me-2"></i>09123456789 (SMART)</p>
-            <p><i class="bi bi-telephone-fill me-2"></i>09123456789 (SMART)</p>
-            <p><i class="bi bi-telephone-fill me-2"></i>09123456789 (SMART)</p>
+                <p><i class="bi bi-telephone-fill me-2"></i>09123456789 (SMART)</p>
+                <p><i class="bi bi-telephone-fill me-2"></i>09123456789 (SMART)</p>
+                <p><i class="bi bi-telephone-fill me-2"></i>09123456789 (SMART)</p>
             </div>
 
             <small class="text-primary">
@@ -72,56 +72,56 @@
         <nav class="navbar navbar-expand border-bottom navbar-light bg-light d-flex flex-row justify-content-between align-items-center p-2">
             <!-- Button for sidebar toggle -->
             <button class="btn toggle-btn" type="button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="blue" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M1 2.5h14v1H1v-1zm0 5h14v1H1v-1zm0 5h14v1H1v-1z"/>
-    </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="blue" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 2.5h14v1H1v-1zm0 5h14v1H1v-1zm0 5h14v1H1v-1z" />
+                </svg>
             </button>
 
             <div class="d-flex flex-row align-items-center justify-content-center gap-4">
                 <!--NOTIFICATIONS-->
                 <div class="dropdown">
-                
-                <button type="button" class="btn bellBtn position-relative" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="bi bi-bell-fill fs-4"></i>
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style="margin-top: 15px;">
-                        <?php echo $unread_count ?? "0"; ?>
-                    </span>
-                      <span class="visually-hidden">unread messages</span>
-                    
-                  </button>
-                  <div class="dropdown-menu dropdown-menu-end pt-0 notif-container" aria-labelledby="dropdownMenuButton">
-                    
-                    <div class="sticky-top d-flex flex-column justify-content-center align-items-center p-2 notif-text mb-4">
-                        <h3>Notifications</h3>
-                    </div>
 
-                    <?php if(!$notifications): ?>
-                        <p>No notifications</p> <!-- Message when there are no notifications -->
-                    <?php else: ?>
-                        <?php foreach ($notifications as $notif): ?>
-                            <a href="" class="btn btn-sm">
-                                <div class="row row-cols-3 align-items-center justify-content-evenly mb-3">
-                                    <div class="col-md-2"><img src="../img/DPPAM LOGO.png" alt="" style="width: 50px; height: 50px;"></div>
-                                    <div class="col-md-8">
-                                        <span>
-                                            <?php echo $notif['FROM'] . 
-                                            "<br>" . $notif['MESSAGE'] . 
-                                            "<br>" . $notif['FORMATTED_DATE']; 
-                                            ?>
-                                        </span>
+                    <button type="button" class="btn bellBtn position-relative" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-bell-fill fs-4"></i>
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style="margin-top: 15px;">
+                            <?php echo $unread_count ?? "0"; ?>
+                        </span>
+                        <span class="visually-hidden">unread messages</span>
+
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-end pt-0 notif-container" aria-labelledby="dropdownMenuButton">
+
+                        <div class="sticky-top d-flex flex-column justify-content-center align-items-center p-2 notif-text mb-4">
+                            <h3>Notifications</h3>
+                        </div>
+
+                        <?php if (!$notifications): ?>
+                            <p>No notifications</p> <!-- Message when there are no notifications -->
+                        <?php else: ?>
+                            <?php foreach ($notifications as $notif): ?>
+                                <a href="" class="btn btn-sm">
+                                    <div class="row row-cols-3 align-items-center justify-content-evenly mb-3">
+                                        <div class="col-md-2"><img src="../img/DPPAM LOGO.png" alt="" style="width: 50px; height: 50px;"></div>
+                                        <div class="col-md-8">
+                                            <span>
+                                                <?php echo $notif['FROM'] .
+                                                    "<br>" . $notif['MESSAGE'] .
+                                                    "<br>" . $notif['FORMATTED_DATE'];
+                                                ?>
+                                            </span>
+                                        </div>
+
+                                        <div class="position-relative col-md-1">
+                                            <span class="position-absolute top-0 start-50 translate-middle p-2 bg-success border border-light rounded-circle">
+                                                <span class="visually-hidden">New alerts</span>
+                                            </span>
+                                        </div>
                                     </div>
-                                    
-                                    <div class="position-relative col-md-1">
-                                        <span class="position-absolute top-0 start-50 translate-middle p-2 bg-success border border-light rounded-circle">
-                                            <span class="visually-hidden">New alerts</span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </a>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
-                  </div>
-            </div>
+                                </a>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
 
                 <!--PROFILE-->
 
@@ -138,7 +138,7 @@
                     </button>
                     <ul class="dropdown-menu dropdown-menu-end">
                         <li><a class="dropdown-item" href="#">My Profile</a></li>
-                        <li><a class="dropdown-item" href="#">Change Password</a></li>
+                        <li><button class="dropdown-item" data-bs-toggle="modal" data-bs-target="#changePassModal">Change Password</button></li>
                         <li><a class="dropdown-item" href="/logout">Logout</a></li>
                     </ul>
                 </div>
@@ -146,4 +146,93 @@
         </nav>
 
 
-<script src="js/sidebar_toggle.js"></script>
+        <!-- MODAL: CHANGE PASSWORD -->
+        <div class="modal fade" id="changePassModal" tabindex="-1" aria-labelledby="changePassModallabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header bg-primary">
+                        <h1 class="modal-title fs-5 text-light" id="changePassModallabel">Change Password</h1>
+                    </div>
+                    <div class="modal-body">
+                        <form action="" method="POST">
+
+                            <!-- CURRENT PASSWORD -->
+                            <div class="col mb-5">
+                                <label for="current-password" class="form-label"><span class="text-primary">Current Password</span></label>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="fas fa-lock text-primary"></i></span>
+                                    <input type="password" class="form-control password-input" name="current-password" id="current-password" required>
+                                    <span class="input-group-text toggle-password" style="cursor: pointer;">
+                                        <i class="fas fa-eye text-primary"></i>
+                                    </span>
+                                    <div class="invalid-feedback">
+                                        Please input current password.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- NEW PASSWORD -->
+                            <div class="col mb-3">
+                                <label for="new-password"><span class="text-primary">Create New Password</span></label>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="fas fa-lock text-primary"></i></span>
+                                    <input type="password" class="form-control password-input" name="new-password" id="new-password" required>
+                                    <span class="input-group-text toggle-password" style="cursor: pointer;">
+                                        <i class="fas fa-eye text-primary"></i>
+                                    </span>
+                                    <div class="invalid-feedback">
+                                        Please input new password.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- CONFIRM NEW PASSWORD -->
+                            <div class="col mb-4">
+                                <label for="confirm-new-password"><span class="text-primary">Confirm New Password</span></label>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="fas fa-lock text-primary"></i></span>
+                                    <input type="password" class="form-control password-input" name="confirm-new-password" id="confirm-new-password" required>
+                                    <span class="input-group-text toggle-password" style="cursor: pointer;">
+                                        <i class="fas fa-eye text-primary"></i>
+                                    </span>
+                                    <div class="invalid-feedback">
+                                        Please input confirm password.
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form>
+
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn" data-bs-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary">Change password</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <script>
+            document.querySelectorAll('.toggle-password').forEach(item => {
+                item.addEventListener('click', function() {
+                    const input = this.previousElementSibling; // Selects the input field
+                    const icon = this.querySelector('i'); // Selects the eye icon
+
+                    if (input.type === "password") {
+                        input.type = "text";
+                        icon.classList.remove("fa-eye");
+                        icon.classList.add("fa-eye-slash");
+                    } else {
+                        input.type = "password";
+                        icon.classList.remove("fa-eye-slash");
+                        icon.classList.add("fa-eye");
+                    }
+                });
+            });
+        </script>
+
+
+        <script src="js/sidebar_toggle.js"></script>
